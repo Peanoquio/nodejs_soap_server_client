4bcd26411114f7777f12428509ac7c7aa36dc1dc
Oliver <oliver@circles.asia>
2019-02-12 15:31:38 +0800

    Refactored the SOAP client into a class that will support connection persistence and pooling

--- soap_client.js
+++ soap_client.js
@@ -1,38 +1,110 @@
 const soap = require('soap');
+const request = require('request');
+
+// the options to support conneciton persistence and pooling
+const optionsForConnPersist = {
+    // HTTP agent options
+    agentOptions: {
+        keepAlive: true,
+        // When using the keepAlive option, specifies the initial delay for TCP Keep-Alive packets. 
+        // Ignored when the keepAlive option is false or undefined.
+        keepAliveMsecs: 60000,
+        // By default set to 256. For agents with keepAlive enabled, this sets the maximum number of sockets that will be left open in the free state.
+        maxFreeSockets: 1,
+    },
+    // follow HTTP 3xx responses as redirects
+    followRedirect: true,
+    followAllRedirects: true,
+    // keep-alive 
+    forever: true,
+    // integer containing number of milliseconds, controls two timeouts
+    // Time to wait for a server to send response headers (and start the response body) before aborting the request. 
+    // Note that if the underlying TCP connection cannot be established, the OS-wide TCP connection timeout will overrule the timeout option 
+    // (the default in Linux can be anywhere from 20-120 seconds).
+    // Sets the socket to timeout after timeout milliseconds of inactivity on the socket.
+    timeout: 10000,
+    // if true, the request-response cycle (including all redirects) is timed at millisecond resolution.
+    time: true,
+};
+
+// Note that if you are sending multiple requests in a loop and creating multiple new pool objects, 
+// maxSockets will not work as intended. To work around this, either use request.defaults with your pool options 
+// or create the pool object with the maxSockets property outside of the loop.
+const optionsForConnPool = {
+    // connection pooling
+    pool: {
+        // maxSockets property can also be provided on the pool object to set the max number of sockets for all agents 
+        maxSockets: 1,
+    },
+};
+
+const optionsForConnPersistAndPool = Object.assign(optionsForConnPool, optionsForConnPersist);
+
+// persistent request object (based on the specified options)
+const persistentRequest = request.defaults(optionsForConnPersistAndPool);
+
+const options = {
+    // override the request module
+    request: persistentRequest
+};
 
 const url = 'http://127.0.0.1:8001/soap?wsdl';
 
-const nickname = 'Peanoquio';
 
-// create the SOAP client
-soap.createClientAsync(url).then(async (client) => {
-    console.log('describe the available SOAP services that the client can access', client.describe());
-
-    // NOTE: the SOAP functions in the server are automatically generated with the asynchronous versions of it 
-    // (appended with Async suffix)
-
-    const message = `Hi there ${nickname}, the time is ${new Date().getTime()}`;
-    let result = await client.PostMessageAsync({ nickname, message });
-
-    // result is a javascript array containing result, rawResponse, soapheader, and rawRequest
-    // result is a javascript object
-    // rawResponse is the raw xml response string
-    // soapHeader is the response soap header as a javascript object
-    // rawRequest is the raw xml request string
-    console.log('SOAP client call PostMessage result:', result);
-
-    const jsonObj = result[0];
-    if (jsonObj && jsonObj.hasOwnProperty('resultcode')) {
-        if (jsonObj.resultcode) {
-             result = await client.GetNumOfMessagesAsync({ nickname });
-             console.log('SOAP client call GetNumOfMessages result:', result);
-
-             const msgIndex = result[0] ? (result[0].number - 1) : 0;
-             result = await client.RetrieveMessageAsync({ nickname, number: msgIndex });
-             console.log('SOAP client call RetrieveMessage result:', result);
+/**
+ * The SOAP client factory class
+ */
+class SoapClientFactory {
+    /**
+     * Create the SOAP client
+     */
+    async createSoapClient() {
+        try {
+            const client = await soap.createClientAsync(url, options);
+            this.attachEventListeners(client);
+            console.log('describe the available SOAP services that the client can access', client.describe());
+            return client;
+        } catch(err) {
+            console.error('SOAP client error:', err);
         }
     }
 
-}).catch(err => {
-    console.error('SOAP client call error:', err);
-});
+    /**
+     * Attach the event listeners to the SOAP client
+     * @param {Object} client 
+     */
+    attachEventListeners(client) {
+        // SOAP client event listeners
+        // Emitted before a request is sent
+        client.on('request', (xml, eid) => {
+            console.log('SOAP client request event');
+            console.log('eid:', eid, 'xml:', xml);
+        });
+        // Emitted before a request is sent, but only the body is passed to the event handler
+        client.on('message', (message, eid) => {
+            console.log('SOAP client message event');
+            console.log('eid:', eid, 'message:', message);
+        });
+        // Emitted when an erroneous response is received
+        client.on('soapError', (error, eid) => {
+            console.error('SOAP client soapError event');
+            console.error('eid:', eid, 'error:', error);
+        });
+        // Emitted after a response is received. This is emitted for all responses (both success and errors). 
+        client.on('response', (body, response, eid) => {
+            console.log('SOAP client response event');
+            console.log('eid:', eid, 'body:', body);
+            console.log('response.req.socket.connecting', response.req.socket.connecting);
+            console.log('response.request.agentOptions', response.request.agentOptions);
+            console.log('response.request.pool', response.request.pool);
+        });
+    }
+} // end class
+
+
+const soapClientFactory = new SoapClientFactory();
+module.exports = { 
+    soapClientFactory,
+    optionsForConnPersist,
+    optionsForConnPool,
+};
